6c9479a3bb09a776dadd8faa7dd3ddb647f1595e
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2022-10-19 15:58:38 -0700

    modified hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const navList = document.querySelector('.nav-list');
+console.log(hamburger);
+
+
 
 hamburger.addEventListener("click", ()=>{
   hamburger.classList.toggle("active");
@@ -10,3 +13,5 @@ document.querySelectorAll(".nav-list").forEach(n => n.addEventListener("click",(
   hamburger.classList.remove("active");
   navList.classList.remove("active");
 }))
+
+console.log(hamburger);
